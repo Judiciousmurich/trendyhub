5eb6a4944c1a6722a7fe2510eb77fca0871c4d71
judiciousmurich <murichjudicious45@gmail.com>
2023-07-14 16:25:11 +0300

    admin

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,8 @@
 
 
 import { RouterProvider } from "react-router-dom";
-import { router } from "./routes/Routes";
+import { router } from "./routes/route";
+// import { router } from "./routes/Routes";
 
 
 const App = () => {
