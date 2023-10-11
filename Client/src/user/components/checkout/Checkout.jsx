398e8361ae851126659af794d0cb4ece89db3129
judiciousmurich <murichjudicious45@gmail.com>
2023-10-11 23:51:32 +0300

    add styles

--- Client/src/user/components/checkout/Checkout.jsx
+++ Client/src/user/components/checkout/Checkout.jsx
@@ -3,7 +3,6 @@ import { RiCheckboxCircleLine } from "react-icons/ri";
 import { Link } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Clients from "../../../shared/Clients";
 
 const Checkout = () => {
   const [shoppingFrequency, setShoppingFrequency] = useState("");
